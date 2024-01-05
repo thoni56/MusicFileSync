4224e09540292c1c7d921e29076f8f55c568c69e
Thomas Nilefalk <thomas@nilefalk.se>
2024-01-05 11:39:29 +0100

    Restructure how mocks are set up

--- musicfilesync_test.py
+++ musicfilesync_test.py
@@ -1,18 +1,24 @@
+import unittest
 from unittest.mock import patch
 
 from musicfilesync import sync
 
-@patch("musicfilesync.update")
-@patch("musicfilesync.get_source_file_list")
-def test_empty_source_and_destination_will_give_no_updates_or_deletes(get_source_file_list_mock, update_mock):
-    update_mock.return_value = []
-    assert sync("source", "destination") == ([], [])
+class TestMusicFileSync(unittest.TestCase):
 
-@patch("musicfilesync.update")
-@patch("musicfilesync.get_source_file_list")
-def test_one_file_in_source_will_give_that_as_updates(get_source_file_list_mock, update_mock):
-    update_mock.return_value = ["aFile.mp3"]
-    assert sync("source", "destination") == (["aFile.mp3"], [])
+    def setUp(self):
+        self.patcher_for_get_source_file_list = patch("musicfilesync.get_source_file_list")
+        self.get_source_file_list_mock = self.patcher_for_get_source_file_list.start()
+        self.patcher_for_update = patch("musicfilesync.update")
+        self.update_mock = self.patcher_for_update.start()
 
-# More tests:
-# - filtering conditions to be propagated to source_file_lister
+    def test_empty_source_and_destination_will_give_no_updates_or_deletes(self):
+        self.update_mock.return_value = []
+        assert sync("source", "destination") == ([], [])
+
+    def test_one_file_in_source_will_give_that_as_updates(self):
+        self.update_mock.return_value = ["aFile.mp3"]
+        assert sync("source", "destination") == (["aFile.mp3"], [])
+
+    # More tests:
+    # - filtering conditions to be propagated to source_file_lister
+    #
