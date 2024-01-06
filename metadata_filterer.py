f59eabd43d19d44ac481d922acc0e1fc2f3ffd6e
Thomas Nilefalk <thomas@nilefalk.se>
2024-01-06 22:53:34 +0100

    Add a metadata_filterer with tests

--- metadata_filterer.py
+++ metadata_filterer.py
@@ -1,2 +1,14 @@
+import audio_metadata
+
 def metadata_filter(files, metadata_filter):
-    assert False, "Not implemented yet"
+    if (metadata_filter != {} and files != []):
+        filtered_files = []
+        for file in files:
+            metadata = audio_metadata.load(file)
+            for tagname, expected_value in metadata_filter.items():
+                actual_value = getattr(metadata.tag, tagname, [])
+                if (actual_value[0] == expected_value):
+                    filtered_files.append(file)
+        return filtered_files
+    else:
+        return files
--- metadata_filterer_test.py
+++ metadata_filterer_test.py
@@ -1,15 +1,32 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
-from musicfilesync import sync
+from metadata_filterer import metadata_filter
 
 class TestMetadataFiltererTest(unittest.TestCase):
 
     def setUp(self):
-        self.patcher_for_glob_glob = patch("metadata_filterer...")
-        self.glob_glob_mock = self.patcher_for_glob_glob.start()
-        self.glob_glob_mock.return_value = []
+        self.patcher_for_load_metadata = patch("metadata_filterer.audio_metadata.load")
+        self.load_metadata_mock = self.patcher_for_load_metadata.start()
+        self.load_metadata_mock.return_value = MagicMock()
         pass
 
-    def test_nothing_yet(self):
-        pass
+    def test_returns_no_files_if_no_files(self):
+        self.assertEqual(metadata_filter([], {}), [])
+
+    def test_returns_all_files_with_no_filter(self):
+        self.assertEqual(metadata_filter(["a", "b", "c"], {}), ["a", "b", "c"])
+
+    def test_return_only_files_with_matching_single_metadata(self):
+        metadata_for_a = MagicMock()
+        metadata_for_a.tag.artist = ["a"]
+
+        metadata_for_b = MagicMock()
+        metadata_for_b.tag.artist = ["b"]
+
+        metadata_for_c = MagicMock()  
+        metadata_for_c.tag.artist = ["c"]
+
+        self.load_metadata_mock.side_effect = [metadata_for_a, metadata_for_b, metadata_for_c]
+
+        self.assertEqual(metadata_filter(["a", "b", "c"], {"artist": "a"}), ["a"])
