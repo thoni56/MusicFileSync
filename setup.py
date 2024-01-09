ad69bbca3f79102763a88f686b0f7fd05e563659
Thomas Nilefalk <thomas@nilefalk.se>
2024-01-09 01:14:10 +0100

    Add dependency on 'tinytag' to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='musicfilesync',
     version='0.1',
     packages=find_packages(),
+    install_requires=[
+        'tinytag'
+    ],
     entry_points={
         'console_scripts': [
             'musicfilesync = musicfilesync.musicfilesync:main',
